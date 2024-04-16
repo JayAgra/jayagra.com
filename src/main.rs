783b258eb25909f8864cf86cb1472bd4b6022ae7
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-04-16 12:26:41 -0700

    filesgit add --all

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use actix_governor::{Governor, GovernorConfigBuilder};
+use actix_web::{ middleware::{self, DefaultHeaders}, web, App, HttpServer };
+use actix_web_static_files::ResourceFiles;
+use dotenv::dotenv;
+use openssl::{
+    ssl::{SslAcceptor, SslFiletype, SslMethod},
+    x509::X509,
+};
+use reqwest;
+use std::{env, io};
+
+mod static_files;
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // load environment variables from .env file
+    dotenv().ok();
+
+    // don't log all that shit when in release mode
+    if cfg!(debug_assertions) {
+        env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    } else {
+        env_logger::init_from_env(env_logger::Env::new().default_filter_or("error"));
+        println!("[OK] starting in release mode");
+    }
+
+    // ratelimiting with governor
+    let governor_conf = GovernorConfigBuilder::default()
+        // these may be a lil high but whatever
+        .per_nanosecond(100)
+        .burst_size(25000)
+        .finish()
+        .unwrap();
+
+    /*
+     *  generate a self-signed certificate for localhost (run from bearTracks directory):
+     *  openssl req -x509 -newkey rsa:4096 -nodes -keyout ./ssl/key.pem -out ./ssl/cert.pem -days 365 -subj '/CN=localhost'
+     */
+    // create ssl builder for tls config
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file("./ssl/key.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("./ssl/cert.pem").unwrap();
+    let intermediate_cert_url = "https://letsencrypt.org/certs/lets-encrypt-r3.der";
+    let intermediate_bytes = reqwest::blocking::get(intermediate_cert_url).unwrap().bytes().unwrap();
+    let intermediate_cert = X509::from_der(&intermediate_bytes).unwrap();
+    builder.add_extra_chain_cert(intermediate_cert).unwrap();
+
+    // config done. now, create the new HttpServer
+    log::info!("[OK] starting jayagra.com on port 443 and 80");
+
+        HttpServer::new(move || {
+        // generated resources from actix_web_files
+        let generated = generate();
+        App::new()
+            // use governor ratelimiting as middleware
+            .wrap(Governor::new(&governor_conf))
+            // logging middleware
+            .wrap(middleware::Logger::default())
+            // default headers for caching. overridden on most all api endpoints
+            .wrap(
+                DefaultHeaders::new()
+                    .add(("Cache-Control", "public, max-age=23328000"))
+                    .add(("X-bearTracks", "5.2.0")),
+            )
+            // html
+            .route("/", web::get().to(static_files::static_index))
+            .route("/minesweeper", web::get().to(static_files::static_minesweeper))
+            .route("/snek", web::get().to(static_files::static_snek))
+            .route("/2048", web::get().to(static_files::static_2048))
+            .route("/2048_8", web::get().to(static_files::static_2048_8))
+            .route("/base64", web::get().to(static_files::static_base64))
+            .route("/pwd", web::get().to(static_files::static_pw))
+            .route("/support{args}*", web::get().to(static_files::static_support))
+            .route("/legal/privacy{args}*", web::get().to(static_files::static_privacy))
+            .route("/legal/cookies{args}*", web::get().to(static_files::static_cookies))
+            .route("/wordle.min.js", web::get().to(static_files::static_wordle_js))
+            .route("/site.webmanifest", web::get().to(static_files::static_site_webmanifest))
+            .route("/sitemap.xml", web::get().to(static_files::static_sitemap_xml))
+            // icons
+            .route("/android-chrome-192x192.png", web::get().to(static_files::static_android_chrome_192))
+            .route("/apple-touch-icon.png", web::get().to(static_files::static_apple_touch_icon))
+            .route("/favicon-16x16.png", web::get().to(static_files::static_favicon_16))
+            .route("/favicon-32x32.png", web::get().to(static_files::static_favicon_32))
+            .route("/favicon.ico", web::get().to(static_files::static_favicon))
+            // public/static
+            .service(ResourceFiles::new("/static", generated))
+        })
+        .bind_openssl(format!("{}:443", env::var("HOSTNAME").unwrap_or_else(|_| "localhost".to_string())), builder)?
+        .bind((env::var("HOSTNAME").unwrap_or_else(|_| "localhost".to_string()), 80))?
+        .workers(8)
+        .run()
+        .await
+}
---
+++ src/static_files.rs
@@ -0,0 +1,127 @@
+use actix_web::{
+    http::header::{CacheControl, CacheDirective, ContentType},
+    HttpRequest, HttpResponse,
+};
+use std::{env, include_bytes, include_str};
+
+const INDEX_HTML: &str = include_str!("../public/index.html");
+const MINESWEEPER_HTML: &str = include_str!("../public/minesweeper.html");
+const SNEK_HTML: &str = include_str!("../public/snek.html");
+const TWENTY48_HTML: &str = include_str!("../public/2048.html");
+const TWENTY48_8_HTML: &str = include_str!("../public/2048_8.html");
+const BASE64_HTML: &str = include_str!("../public/base64.html");
+const PW_HTML: &str = include_str!("../public/pw.html");
+const APP_SUPPORT_HTML: &str = include_str!("../public/support.html");
+const PRIVACY_HTML: &str = include_str!("../public/privacy.html");
+const COOKIES_HTML: &str = include_str!("../public/cookies.html");
+const WORDLE_JS: &str = include_str!("../public/wordle.min.js");
+const SITEMAP_XML: &str = include_str!("../public/sitemap.xml");
+const SITE_WEBMANI: &str = include_str!("../public/site.webmanifest");
+// icons aren't utf8 so it needs include bytes instead
+const ANDROID_CHROME_192: &[u8] = include_bytes!("../public/android-chrome-192x192.png");
+// const ANDROID_CHROME_512: &[u8] = include_bytes!("../public/android-chrome-512x512.png");
+const APPLE_TOUCH_ICON: &[u8] = include_bytes!("../public/apple-touch-icon.png");
+const FAVICON_16: &[u8] = include_bytes!("../public/favicon-16x16.png");
+const FAVICON_32: &[u8] = include_bytes!("../public/favicon-32x32.png");
+const FAVICON_ICO: &[u8] = include_bytes!("../public/favicon.ico");
+
+pub async fn static_index(req: HttpRequest) -> HttpResponse {
+    // redirect requests not to port 443 (port 80) to 443
+    match req.app_config().local_addr().port() {
+        443 => {
+            HttpResponse::Ok()
+                .content_type(ContentType::html())
+                .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(23328000u32)]))
+                .body(INDEX_HTML)
+        }
+        _ => {
+            HttpResponse::PermanentRedirect()
+                .append_header(("location", format!("https://{}", env::var("HOSTNAME").unwrap_or_else(|_| "localhost".to_string()))))
+                .finish()
+        }
+    }
+}
+
+pub async fn static_minesweeper() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(MINESWEEPER_HTML)
+}
+
+pub async fn static_snek() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(SNEK_HTML)
+}
+
+pub async fn static_2048() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(TWENTY48_HTML)
+}
+
+pub async fn static_2048_8() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(TWENTY48_8_HTML)
+}
+
+pub async fn static_base64() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(BASE64_HTML)
+}
+
+pub async fn static_pw() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(PW_HTML)
+}
+
+pub async fn static_support() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(APP_SUPPORT_HTML)
+}
+
+pub async fn static_privacy() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(PRIVACY_HTML)
+}
+
+pub async fn static_cookies() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(COOKIES_HTML)
+}
+
+pub async fn static_wordle_js() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(WORDLE_JS)
+}
+
+pub async fn static_sitemap_xml() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(SITEMAP_XML)
+}
+
+pub async fn static_site_webmanifest() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::html()).body(SITE_WEBMANI)
+}
+
+// serve static favicons
+pub async fn static_android_chrome_192() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/png"))
+        .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(4838400u32)]))
+        .body(ANDROID_CHROME_192)
+}
+
+pub async fn static_apple_touch_icon() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/png"))
+        .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(4838400u32)]))
+        .body(APPLE_TOUCH_ICON)
+}
+
+pub async fn static_favicon_16() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/png"))
+        .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(4838400u32)]))
+        .body(FAVICON_16)
+}
+
+pub async fn static_favicon_32() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/png"))
+        .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(4838400u32)]))
+        .body(FAVICON_32)
+}
+
+pub async fn static_favicon() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/x-icon"))
+        .insert_header(CacheControl(vec![CacheDirective::Public, CacheDirective::MaxAge(4838400u32)]))
+        .body(FAVICON_ICO)
+}
